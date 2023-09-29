7a3fef44a88ebdc98bbc46105b9cbfa06bb80dd9
Jason Luther <jason@ixid.net>
2023-09-29 19:03:50 -0400

    Add script to install python dependencies

---
+++ Code/Python/setup-macos.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+brew install python-tk
+pip3 install customtkinter
+pip3 install serial
+pip3 install pyserial
+pip3 install keyboard
+pip3 install pygetwindow
+pip3 install numpy
+pip3 install opencv-python
